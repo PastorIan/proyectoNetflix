146dc7543cf771f19e12fcb48c83fce6882327ea
Ian <iandope20@gmail.com>
2020-03-24 00:18:24 -0400

    Ejemplo hola mundo corriendo

---
+++ src/main/java/com/ian/proyecto/backend/TestControler.java
@@ -0,0 +1,18 @@
+package com.ian.proyecto.backend;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestControler {
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String>helloWorld(){
+        return new ResponseEntity<>("hola Mundo", HttpStatus.OK);
+    }
+}
